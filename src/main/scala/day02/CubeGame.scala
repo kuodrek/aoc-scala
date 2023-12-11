b731ead8b3ed1f001d47ce2c764ad7719e4f8479
kuodrek <kuodrek@gmail.com>
2023-12-10 23:32:45 -0300

    Develop extractColor function

--- src/main/scala/day02/CubeGame.scala
+++ src/main/scala/day02/CubeGame.scala
@@ -6,20 +6,17 @@ import scala.util.matching.Regex
 case class CubeGame(game: List[String]) {
   private val numberPattern: Regex = "\\d+".r
   private val extractColor: String => Int = color => {
-    val filteredGame: String = Try {
-      game.find(line => line.contains(color)).toString
-    }.getOrElse("0")
-    val cubeNumber: Option[String] = numberPattern.findFirstIn(filteredGame)
-    val result = cubeNumber match {
-      case Some(value) => value.toInt
+    val splitGame: List[String] = game.flatMap(cubeColor => cubeColor.split(","))
+    val resultOption = splitGame.find(line => line.contains(color))
+    resultOption match {
+      case Some(result) => numberPattern.findFirstIn(result).map(_.toInt).getOrElse(0)
       case None => 0
     }
-    result
   }
 
   val redCubes: Int = extractColor("red")
   val greenCubes: Int = extractColor("green")
   val blueCubes: Int = extractColor("blue")
 
-  override def toString: String = s"Red cubes: $redCubes, Green cubes: $greenCubes, Blue cubes: $blueCubes"
+  override def toString: String = s";;Red cubes: $redCubes, Green cubes: $greenCubes, Blue cubes: $blueCubes;;"
 }
