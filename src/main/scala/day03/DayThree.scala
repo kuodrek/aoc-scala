24ad62ffae160a0df562271297e56d2285d3394d
kuodrek <kuodrek@gmail.com>
2023-12-20 23:45:49 -0300

    finish part one

--- src/main/scala/day03/DayThree.scala
+++ src/main/scala/day03/DayThree.scala
@@ -4,7 +4,14 @@ import scala.util.matching.Regex
 import scala.util.matching.Regex.Match
 
 case class Coordinates(x: Int, y: Int)
-case class Symbol(value: String, coordinates: Coordinates)
+
+case class Symbol(value: String, coordinates: Coordinates) {
+  def checkForNumber(number: Number): Boolean = {
+    if (coordinates.x < number.start.x-1 || coordinates.x > number.end.x+1) false
+    else if (coordinates.y < number.start.y-1 || coordinates.y > number.end.y+1) false
+    else true
+  }
+}
 case class Number(value: Int, start: Coordinates, end: Coordinates)
 object IsInt {
   def unapply(in: Match): Option[Int] = in.matched.toIntOption
@@ -13,16 +20,23 @@ object IsInt {
 object DayThree {
   private def PartOne(textFile: Iterator[String]): Int = {
     val extractor: Regex = """\d+|[^.\d]""".r
-    val engineSchematic = textFile.mkString.split("\n").zipWithIndex.flatMap {
+//    val text = textFile.mkString
+    val engineSchematic = textFile.zipWithIndex.flatMap {
       case (line, i) => extractor.findAllMatchIn(line).map {
-        case num @ IsInt(nb) => Number(nb, Coordinates(num.start, i), Coordinates(num.end, i))
+        case num @ IsInt(nb) => Number(nb, Coordinates(num.start, i), Coordinates(num.end - 1, i))
         case sym => Symbol(sym.matched, Coordinates(sym.start, i))
       }
+    }.toArray
+    val symbols = engineSchematic.collect {
+      case sym: Symbol => sym
     }
-    val sum = engineSchematic.map {
-      case num: Number => num.value // need to check if num is in neighborhood of symbol
+    val numbers = engineSchematic.collect {
+      case num: Number => num
+    }
+
+    numbers.collect {
+      case num: Number if symbols.exists(_.checkForNumber(num)) => num.value
     }.sum
-    0
   }
   def main(args: Array[String]): Unit = {
     // Get input
@@ -32,6 +46,6 @@ object DayThree {
     val filename: String = "./src/main/scala/day03/input/data.txt"
     val textFile: BufferedSource = Source.fromFile(filename)
     val totalSum: Int = PartOne(textFile.getLines)
-//    val pattern
+    println(totalSum)
   }
 }
