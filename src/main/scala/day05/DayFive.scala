b27d63a917b5e6307189e829aeb54c7913eaf1d2
kuodrek <kuodrek@gmail.com>
2024-01-10 22:56:30 -0300

    move SourceToDestination to object dayfive

--- src/main/scala/day05/DayFive.scala
+++ src/main/scala/day05/DayFive.scala
@@ -9,19 +9,18 @@ case class DestinationMap(destination: Int, source: Int, length: Int) {
   def GetDestination(candidate: Int): Int = candidate - (source - destination)
 }
 
-def SourceToDestination(candidate: Int, destinations: List[DestinationMap]) : Int = {
-  val destination: Option[DestinationMap] = destinations.find(_.CheckSourceCandidate(candidate))
-  println(destination)
-  destination match {
-    case Some(destinationMap) => destinationMap.GetDestination(candidate)
-    case None => candidate
-  }
-}
-
 object DayFive {
   private def PartOne(): Int = {0}
   private def PartTwo(): Int = {0}
-  private def SourceToDestination(source: Int, destinations: List[DestinationMap]) : Int = {0}
+  private def SourceToDestination(candidate: Int, destinations: List[DestinationMap]) : Int = {
+    val destination: Option[DestinationMap] = destinations.find(_.CheckSourceCandidate(candidate))
+    println(destination)
+    destination match {
+      case Some(destinationMap) => destinationMap.GetDestination(candidate)
+      case None => candidate
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val filename: String = "./src/main/scala/day05/input/data.txt"
     val textFile: BufferedSource = Source.fromFile(filename)
