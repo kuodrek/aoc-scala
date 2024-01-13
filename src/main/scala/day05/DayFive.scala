44980d0da3321e4a59be5acc6fe168331170e677
kuodrek <kuodrek@gmail.com>
2024-01-12 23:32:20 -0300

    add part one

--- src/main/scala/day05/DayFive.scala
+++ src/main/scala/day05/DayFive.scala
@@ -2,20 +2,30 @@ package day05
 import scala.io.{BufferedSource, Source}
 import scala.util.matching.Regex
 
-case class DestinationMap(destination: Int, source: Int, length: Int) {
-  def CheckSourceCandidate(candidate: Int): Boolean = {
-    if (candidate >= source && source <= (source + length)) true
+case class DestinationMap(destination: Long, source: Long, length: Long) {
+  def CheckSourceCandidate(candidate: Long): Boolean = {
+    if (candidate >= source && candidate <= (source + length)) true
     else false
   }
-  def GetDestination(candidate: Int): Int = candidate - (source - destination)
+  def GetDestination(candidate: Long): Long = candidate - (source - destination)
 }
 
 object DayFive {
-  private def PartOne(): Int = {0}
-  private def PartTwo(): Int = {0}
-  private def SourceToDestination(candidate: Int, destinations: List[DestinationMap]) : Int = {
+  private def PartOne(seeds: Array[Long], destinations: Array[Array[DestinationMap]]): Long = {
+    val soils: Array[Long] = seeds.map(seed => SourceToDestination(seed, destinations(0)))
+    val fertilizers: Array[Long] = soils.map(soil => SourceToDestination(soil, destinations(1)))
+    val waters: Array[Long] = fertilizers.map(fertilizer => SourceToDestination(fertilizer, destinations(2)))
+    val lights: Array[Long] = waters.map(water => SourceToDestination(water, destinations(3)))
+    val temperatures: Array[Long] = lights.map(light => SourceToDestination(light, destinations(4)))
+    val humidities: Array[Long] = temperatures.map(temperature => SourceToDestination(temperature, destinations(5)))
+    val locations: Array[Long] = humidities.map(humidity => SourceToDestination(humidity, destinations(6)))
+    locations.min
+  }
+  private def PartTwo(seeds: Array[Long], destinations: Array[Array[DestinationMap]]): Long = {
+    0
+  }
+  private def SourceToDestination(candidate: Long, destinations: Array[DestinationMap]) : Long = {
     val destination: Option[DestinationMap] = destinations.find(_.CheckSourceCandidate(candidate))
-    println(destination)
     destination match {
       case Some(destinationMap) => destinationMap.GetDestination(candidate)
       case None => candidate
@@ -25,13 +35,28 @@ object DayFive {
   def main(args: Array[String]): Unit = {
     val filename: String = "./src/main/scala/day05/input/data.txt"
     val textFile: BufferedSource = Source.fromFile(filename)
-    val gardenData = textFile.mkString.split("\\n")
+    val gardenData = textFile.mkString.split("\\n\\n")
 
     val extractor: Regex = "\\d+".r
-    println(gardenData)
+
     val seeds = extractor.findAllMatchIn(gardenData.head)
-      .flatMap(num => num.matched.toLongOption).toList
-    val destinations = gardenData.drop(1).map(gardenMap => gardenMap.head -> extractor.findAllMatchIn(gardenMap))
-    println(destinations)
+      .flatMap(num => num.matched.toLongOption).toArray
+
+    val destinations: Array[Array[DestinationMap]] = {
+      val pattern = """(\d+)\s+(\d+)\s+(\d+)""".r
+      val gardenMaps: Array[Array[String]] = gardenData.drop(1).map(gardenMap => gardenMap.split(":"))
+      val splitDestinationMaps = gardenMaps.map(
+        gardenMap => gardenMap.last.split("\n").tail
+      )
+      splitDestinationMaps.map(
+        array => array.map {
+          case pattern(m1, m2, m3) => DestinationMap(m1.toLong, m2.toLong, m3.toLong)
+        }
+      )
+    }
+
+    val minLocation = PartOne(seeds, destinations)
+    val minLocation2 = PartTwo(seeds, destinations)
+    println(minLocation)
   }
 }
